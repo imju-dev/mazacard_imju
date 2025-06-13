7d9fd011c796932e1e754f2b2a98a8eca5de8962
Jorge Jared Torres Placencia <2022030143@upsin.edu.mx>
2025-06-12 21:55:38 -0700

    Se actualizó DB

--- db.js
+++ db.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER,              
+    password: process.env.DB_PASSWORD,                           
     database: process.env.DB_NAME,
+    port: 57361,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
